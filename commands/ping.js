4a38ad3f49f03dd0441313a0f233eab9186ffccb
Kabi <kabilan.siv@gmail.com>
2021-03-26 14:31:48 +0100

    added snippet and added error handling in ping command.

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,19 @@ module.exports = {
 	name: 'ping',
 	description: 'This is a ping command.',
 	execute(message, args) {
-		if (message.member.roles.cache.some(r => r.name === 'Devs')) {
-			let successResponse = 'PONG'
-			message.channel.send(successResponse)
-		} else {
-			let errorResponse = 'Nur die Babas aller Babas können pingen akho...'
+		try {
+			if (message.member.roles.cache.some(r => r.name === 'Devs')) {
+				let successResponse = 'PONG'
+				message.channel.send(successResponse)
+			} else {
+				let failureResponse = 'Nur die Babas aller Babas können pingen akho...'
+				message.channel.send(failureResponse)
+			}
+		} catch (error) {
+			console.error(error)
+			let errorResponse = "Zellenwärter ruft mich uno momento ahki..."
 			message.channel.send(errorResponse)
 		}
+
 	}
 }

051317b13ec9bf11c9a29cba24293f3012dca98e
Kabi <kabilan.siv@gmail.com>
2021-08-12 22:22:23 +0200

    leave and play commands now require admin rights.

--- commands/admin/leave.js
+++ commands/admin/leave.js
@@ -1,12 +1,19 @@
 module.exports = {
-    name: 'leave',
-    description: 'stop the bot and leave the channel',
-    async execute(message, args) {
-        const voiceChannel = message.member.voice.channel;
- 
-        if(!voiceChannel) return message.channel.send("Du musst in einem Chat sein um mich zu stoppen...!");
-        await voiceChannel.leave();
-        await message.channel.send('Bruda muss los... :smiling_face_with_tear:')
- 
-    }
+	name: 'leave',
+	description: 'stop the bot and leave the channel',
+	async execute(message, args) {
+		try {
+			if (message.member.roles.cache.some(r => r.name === 'Admin')) {
+				const voiceChannel = message.member.voice.channel;
+
+				if (!voiceChannel) return message.channel.send("Du musst in einem Chat sein um mich zu stoppen...!");
+				await voiceChannel.leave();
+				await message.channel.send('Bruda muss los... :smiling_face_with_tear:')
+			} else {
+				message.channel.send('Muss gehen Fard ruft an. Er meint er braucht doch Hilfe... :smiling_face_with_tear:')
+			}
+		} catch (error) {
+
+		}
+	}
 }
--- commands/admin/play.js
+++ commands/admin/play.js
@@ -2,76 +2,82 @@ const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 
 module.exports = {
-	name: 'play',
-	description: 'Joins and plays a video from youtube',
-	async execute(message, args) {
-		try {
-			const voiceChannel = message.member.voice.channel;
+		name: 'play',
+		description: 'Joins and plays a video from youtube',
+		async execute(message, args) {
+			try {
+				if (message.member.roles.cache.some(r => r.name === 'Admin')) {
+						const voiceChannel = message.member.voice.channel;
 
-			if (!voiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
-			const permissions = voiceChannel.permissionsFor(message.client.user);
-			if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
-			if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
-			if (!args.length) return message.channel.send('You need to send the second argument!');
+						if (!voiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
+						const permissions = voiceChannel.permissionsFor(message.client.user);
+						if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
+						if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
+						if (!args.length) return message.channel.send('You need to send the second argument!');
 
-			const validURL = (str) => {
-				var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-				if (!regex.test(str)) {
-					return false;
-				} else {
-					return true;
+						const validURL = (str) => {
+							var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+							if (!regex.test(str)) {
+								return false;
+							} else {
+								return true;
+							}
+						}
+
+						if (validURL(args[0])) {
+
+							const connection = await voiceChannel.join();
+							const stream = ytdl(args[0])
+
+							connection.play(stream, {
+									seek: 0,
+									volume: 1
+								})
+								.on('finish', () => {
+									voiceChannel.leave();
+									message.channel.send('Bruda muss los... Handyakku fast leer.');
+								});
+
+							await message.reply(`:thumbsup: Jetzt läuft: ***Dein Link!***`)
+
+							return
+						}
+
+
+						const connection = await voiceChannel.join();
+
+						const videoFinder = async (query) => {
+							const videoResult = await ytSearch(query);
+
+							return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+
+						}
+
+						const video = await videoFinder(args.join(' '));
+
+						if (video) {
+							const stream = ytdl(video.url, {
+								filter: 'audioonly'
+							});
+							connection.play(stream, {
+									seek: 0,
+									volume: 1
+								})
+								.on('finish', () => {
+									voiceChannel.leave();
+									message.channel.send('Bruda muss los... Handyakku fast leer.');
+								});
+
+							await message.reply(`:thumbsup: Jetzt läuft ***${video.title}***`)
+						} else {
+							message.channel.send('Keine Videos gefunden.');
+						}
+					} else {
+						message.channel.send('Nur der Baba aller Babas kann mich Schwuchtel zum zwitschern bringen akho...');
+					}
+				}
+				catch (error) {
+					console.log('Unable to run play command');
 				}
 			}
-
-			if (validURL(args[0])) {
-
-				const connection = await voiceChannel.join();
-				const stream = ytdl(args[0])
-
-				connection.play(stream, {
-						seek: 0,
-						volume: 1
-					})
-					.on('finish', () => {
-						voiceChannel.leave();
-						message.channel.send('Bruda muss los... Handyakku fast leer.');
-					});
-
-				await message.reply(`:thumbsup: Jetzt läuft: ***Dein Link!***`)
-
-				return
-			}
-
-
-			const connection = await voiceChannel.join();
-
-			const videoFinder = async (query) => {
-				const videoResult = await ytSearch(query);
-
-				return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
-
-			}
-
-			const video = await videoFinder(args.join(' '));
-
-			if (video) {
-				const stream = ytdl(video.url, {
-					filter: 'audioonly'
-				});
-				connection.play(stream, {
-						seek: 0,
-						volume: 1
-					})
-					.on('finish', () => {
-						voiceChannel.leave();
-					});
-
-				await message.reply(`:thumbsup: Jetzt läuft ***${video.title}***`)
-			} else {
-				message.channel.send('Keine Videos gefunden.');
-			}
-		} catch (error) {
-			console.log('Unable to run play command');
 		}
-	}
-}

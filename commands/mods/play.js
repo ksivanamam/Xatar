387c56566d8b7b750f210838ace93d76ed646a60
Kabi <34021457+ksivanamam@users.noreply.github.com>
2021-10-01 15:37:48 +0200

    removed comments

--- commands/mods/play.js
+++ commands/mods/play.js
@@ -85,33 +85,28 @@ module.exports = {
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 
-//Global queue for your bot. Every server will have a key and value pair in this map. { guild.id, queue_constructor{} }
 const queue = new Map();
 
 module.exports = {
 	name: 'play',
-	aliases: ['skip', 'stop'], //We are using aliases to run the skip and stop command follow this tutorial if lost: https://www.youtube.com/watch?v=QBUJ3cdofqc
+	aliases: ['skip', 'stop'],
 	cooldown: 0,
 	description: 'Advanced music bot',
 	async execute(message, args, cmd, client, Discord) {
 
 
-		//Checking for the voicechannel and permissions (you can add more permissions if you like).
 		const voice_channel = message.member.voice.channel;
 		if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
 		const permissions = voice_channel.permissionsFor(message.client.user);
 		if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
 		if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
 
-		//This is our server queue. We are getting this server queue from the global queue.
 		const server_queue = queue.get(message.guild.id);
 
-		//If the user has used the play command
 		if (cmd === 'play') {
 			if (!args.length) return message.channel.send('You need to send the second argument!');
 			let song = {};
 
-			//If the first argument is a link. Set the song object to have two keys. Title and URl.
 			if (ytdl.validateURL(args[0])) {
 				const song_info = await ytdl.getInfo(args[0]);
 				song = {
@@ -119,7 +114,6 @@ module.exports = {
 					url: song_info.videoDetails.video_url
 				}
 			} else {
-				//If there was no link, we use keywords to search for a video. Set the song object to have two keys. Title and URl.
 				const video_finder = async (query) => {
 					const video_result = await ytSearch(query);
 					return (video_result.videos.length > 1) ? video_result.videos[0] : null;
@@ -136,7 +130,6 @@ module.exports = {
 				}
 			}
 
-			//If the server queue does not exist (which doesn't for the first video queued) then create a constructor to be added to our global queue.
 			if (!server_queue) {
 
 				const queue_constructor = {
@@ -146,11 +139,9 @@ module.exports = {
 					songs: []
 				}
 
-				//Add our key and value pair into the global queue. We then use this to get our server queue.
 				queue.set(message.guild.id, queue_constructor);
 				queue_constructor.songs.push(song);
 
-				//Establish a connection and play the song with the vide_player function.
 				try {
 					const connection = await voice_channel.join();
 					queue_constructor.connection = connection;
@@ -173,7 +164,6 @@ module.exports = {
 const video_player = async (guild, song) => {
 	const song_queue = queue.get(guild.id);
 
-	//If no song is left in the server queue. Leave the voice channel and delete the key and value pair from the global queue.
 	if (!song) {
 		song_queue.voice_channel.leave();
 		queue.delete(guild.id);
@@ -196,7 +186,7 @@ const video_player = async (guild, song) => {
 const skip_song = (message, server_queue) => {
 	if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
 	if (!server_queue) {
-		return message.channel.send(`There are no songs in queue 😔`);
+		return message.channel.send(`There are no songs in queue`);
 	}
 	server_queue.connection.dispatcher.end();
 }

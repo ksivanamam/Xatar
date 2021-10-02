780bf650ced3e51b1222a167eb484e7234c6f43f
Kabi <34021457+ksivanamam@users.noreply.github.com>
2021-10-02 03:52:57 +0200

    fixed aliases

--- events/guild/message.js
+++ events/guild/message.js
@@ -6,7 +6,7 @@ module.exports = (Discord, client, message) => {
 	var args = message.content.slice(prefix.length).split(/ +/)
 	var cmd = args.shift().toLocaleLowerCase()
 
-	const command = client.commands.get(cmd) || client.command.find(a => a.aliases && a.aliases.include(cmd))
+	const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.include(cmd))
 
 	if(command) command.execute(message, args, cmd, client, Discord)
 }

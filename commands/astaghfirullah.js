3139384b9d73f192ffab40cfce19fbda24ae22f2
Kabi <34021457+ksivanamam@users.noreply.github.com>
2021-10-11 19:24:18 +0200

    added astaghfirullah command

---
+++ commands/astaghfirullah.js
@@ -0,0 +1,28 @@
+const queue = new Map();
+const path = require('path')
+
+module.exports = {
+	name: 'astaghfirullah',
+	aliases: ['Istighfar'],
+	cooldown: 0,
+	description: 'Advanced music bot',
+	async execute(message, args, cmd, client, Discord) {
+
+
+		const voice_channel = message.member.voice.channel;
+		if (!voice_channel) return message.channel.send('Du musst in einem Channel sein akho...');
+		const permissions = voice_channel.permissionsFor(message.client.user);
+		if (!permissions.has('CONNECT')) return message.channel.send('Keine Rechte!');
+		if (!permissions.has('SPEAK')) return message.channel.send('Keine Rechte!');
+
+		const server_queue = queue.get(message.guild.id);
+
+		if (cmd === 'astaghfirullah') {
+			voice.cannel.join().then((connection) => {
+				connection.play(path.join(_dirname, 'astaghfirullah.mp3'))
+			})
+
+		}
+	}
+
+}

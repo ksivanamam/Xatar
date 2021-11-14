dddd0c14611790570c6a52355daa2e529367c022
Kabi <34021457+ksivanamam@users.noreply.github.com>
2021-11-14 09:44:40 +0100

    fix 1

--- commands/empathy.js
+++ commands/empathy.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-	name: 'showEmpathy',
-	aliases: ['empathy'],
+	name: 'empathy',
+	aliases: ['mitleid'],
 	cooldown: 0,
 	description: 'Advanced music bot',
 	async execute(message, args, cmd, client, Discord) {
@@ -14,14 +14,14 @@ module.exports = {
 		if (!permissions.has('CONNECT')) return message.channel.send('Keine Rechte!');
 		if (!permissions.has('SPEAK')) return message.channel.send('Keine Rechte!');
 
-		if (cmd === 'showEmpathy') {
+		if (cmd === 'empathy') {
 			let successResponse = 'Liebe afrikanische Kinder und liebe asiatische Kinder es tut mir sehr leid, dass... ** *lacht* **'
 			message.channel.send(successResponse)
 
 			const voiceChannel = message.member.voice.channel;
 			const connection = await voiceChannel.join();
 
-			connection.play(path.join(__dirname, 'dieseMenschen.mp3'))
+			connection.play(path.join(__dirname, 'empathy.mp3'))
 			.on('finish', () => {
 				voiceChannel.leave();
 				message.channel.send('Du hast vollkommen Recht. Das ist nicht witzi...');
